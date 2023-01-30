822f43ffc2c332fc9b5b4e48e864d75bfe773170
suichunyu <2018640800@qq.com>
2023-01-30 03:04:13 -0800

    fix some bugs made by fprintf

--- user/pingpong.c
+++ user/pingpong.c
@@ -10,14 +10,20 @@ int main()
     int fields1[2];//father process sends data to son
     pipe(fields1);
 
-//    int fields2[2];//son process sends data to father
-//    pipe(fields2);
+    int fields2[2];//son process sends data to father
+    pipe(fields2);
 
-    if(fork()>0)
+    int id=fork();
+    if(id<0)
+    {
+        printf("error\n");
+        exit(1);
+    }
+    else if(id>0)
     {
         char send_data[]="received ping";
 
-        printf("write %d bytes\n", write(fields1[1], send_data, sizeof(send_data)));
+        write(fields1[1], send_data, sizeof(send_data));
 
         wait(NULL);
 
@@ -26,31 +32,24 @@ int main()
 
         strcpy(read_buf+ strlen(read_buf),":");
 
-//        read(fields2[0], read_buf + strlen(read_buf), sizeof(read_buf) - sizeof(char) * strlen(read_buf));
+        read(fields2[0], read_buf + strlen(read_buf), sizeof(read_buf) - sizeof(char) * strlen(read_buf));
 
-        fprintf(2,read_buf);
+        printf("<parent> %s",read_buf);
+//        fprintf(2,read_buf); maybe some bugs here
     }
     else//pid==0
     {
-        char test[150];
-
-        int cnt=read(fields1[0], test,14);
-        printf("%d\n",cnt);
-        printf("%s\n",test);
-
         char read_buf[150];
         i2a(getpid(),read_buf);
-
         strcpy(read_buf+ strlen(read_buf),":");
 
-        strcpy(read_buf+ strlen(read_buf),test);
-        //read(fields1[0], read_buf + strlen(read_buf), sizeof(read_buf) - sizeof(char) * strlen(read_buf));
+        read(fields1[0], read_buf + strlen(read_buf), sizeof(read_buf) - sizeof(char) * strlen(read_buf));
 
-        printf("%s\n", read_buf);
+        printf("<child> %s\n", read_buf);
 
-//        char send_data[50]="received pong";
+        char send_data[]="received pong";
 
-//        write(fields2[1], send_data, sizeof(send_data));
+        write(fields2[1], send_data, sizeof(send_data));
 
     }
     exit(0);
