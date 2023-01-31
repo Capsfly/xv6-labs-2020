a15b3dcd56b1e304dc2caf005e67bea2b1a00782
suichunyu <2018640800@qq.com>
2023-01-30 18:27:40 -0800

    test other people's solutions

--- user/primes.c
+++ user/primes.c
@@ -5,11 +5,16 @@
 #include "common_kernel_head.h"
 #include "my_function.c"
 #define EOP (-1)
+
 void seive(int fa2node[2])
 {
     int must_is_prime;
     read(fa2node[0], &must_is_prime, sizeof (must_is_prime));
 
+    if(must_is_prime==EOP)
+    {
+        exit(0);
+    }
     print_prime(must_is_prime);
 
     int node2son[2];
@@ -23,53 +28,49 @@ void seive(int fa2node[2])
     }
     else if(id==0)//son process
     {
+        close(node2son[1]);
+
+        close(fa2node[1]);
+        close(fa2node[0]);
+
+
         seive(node2son);
         exit(0);
     }
     else//parent process
     {
         int now_val;
-        while(read(fa2node[0],&now_val, sizeof(now_val))!=EOP)
+        while(1)
         {
-
+            read(fa2node[0],&now_val, sizeof(now_val));
+            if(now_val==EOP)
+            {
+                break;
+            }
             if(now_val%must_is_prime!=0)
             {
                 write(node2son[1],&now_val, sizeof(now_val));
             }
         }
+        now_val=EOP;
+        write(node2son[1],&now_val, sizeof(now_val));
         exit(0);
     }
 }
 
 int main()
 {
+    int input_pipes[2];
+    pipe(input_pipes);
 
-
-
+    for(int i=2;i<=35;i++)
+    {
+        write(input_pipes[1],&i, sizeof(i));
+    }
+    int eop=EOP;
+    write(input_pipes[1],&eop, sizeof(eop));
+    close(input_pipes[1]);
+    seive(input_pipes);
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
